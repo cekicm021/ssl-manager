021ae918f224f393c46121aadd49a89e0f5a01d1
Nimmly <marko.v.popovic@gmail.com>
2021-12-15 13:44:09 +0100

    some changes

--- app.js
+++ app.js
@@ -5,17 +5,27 @@ const app = express();
 const Greenlock = require("greenlock");
 const greenlock = Greenlock.create({
 	packageRoot: __dirname,
-
-	// contact for security and critical bug notices
+	configDir: "./greenlock.d/",
 	maintainerEmail: "marko@digitalinfinity.rs",
-
-	// where to look for configuration
-	configDir: "./greenlock.d",
-
-	// whether or not to run at cloudscale
-	cluster: false,
+	staging: true,
+	notify: function (event, details) {
+		if ("error" === event) {
+			// `details` is an error object in this case
+			console.error(details);
+		}
+	},
 });
-
+greenlock.manager.defaults({
+	// The "Let's Encrypt Subscriber" (often the same as the maintainer)
+	// NOT the end customer (except where that is also the maintainer)
+	subscriberEmail: "marko@digitalinfinity.rs",
+	agreeToTerms: true,
+	challenges: {
+		"http-01": {
+			module: "acme-http-01-standalone",
+		},
+	},
+});
 function myPlainNodeHttpApp(req, res) {
 	res.end("Hello, Encrypted World!");
 }
@@ -23,7 +33,7 @@ function add(req, res) {
 	greenlock
 		.add({
 			subject: "cekicm.xyz",
-			altnames: ["cekicm.xyz","www.cekicm.xyz"],
+			altnames: ["cekicm.xyz", "www.cekicm.xyz"],
 		})
 		.then((response) => {
 			res.send(response);
@@ -42,5 +52,5 @@ app.get("/test", (req, res) => {
 
 // export the app normally
 // do not .listen()
-app.listen(80,443);
+app.listen(80, 443);
 module.exports = app;
